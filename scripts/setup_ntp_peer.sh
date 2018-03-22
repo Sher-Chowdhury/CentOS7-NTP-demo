727572e4c8e766f2cc783b19c45731605fb27204
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-03-22 15:57:43 +0000

    wip

--- scripts/setup_ntp_peer.sh
+++ scripts/setup_ntp_peer.sh
@@ -6,20 +6,28 @@ echo '##########################################################################
 echo '############### About to run ntp_peer.sh script ##################'
 echo '##########################################################################'
 
+
 yum install -y ntp
 
 
 cp /etc/ntp.conf /etc/ntp.conf-orig
-cp /etc/chrony.conf /etc/chrony.conf-orig
+
+sed -i 's/^server/#server/g' /etc/ntp.conf
+cat 'server 127.127.1.0' >> /etc/ntp.conf
 
 
+systemctl restart ntpd 
+systemctl enable ntpd 
 
-systemctl restart chronyd
-systemctl enable chronyd
-
-
-firewall-cmd --add-service=smtp --permanent
+firewall-cmd --permanent --add-service=ntp
 systemctl restart firewalld.service
 
+systemctl restart ntpd 
+systemctl enable ntpd 
+
+
+# check this has worked by running commands like:
+# ntpq -p
+# ntpstat
 
 exit 0
